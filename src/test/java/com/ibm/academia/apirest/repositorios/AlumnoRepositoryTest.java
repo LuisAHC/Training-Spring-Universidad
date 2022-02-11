d225563514777134653a7d02f5fbfba9a49a010f
LuisAHC <luishceciliano@gmail.com>
2022-02-11 01:37:02 -0600

    Esqueleto prueba AlumnoRepository

---
+++ src/test/java/com/ibm/academia/apirest/repositorios/AlumnoRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.ibm.academia.apirest.repositorios;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlumnoRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void buscarAlumnoPorNombreCarrera() {
+    }
+}
