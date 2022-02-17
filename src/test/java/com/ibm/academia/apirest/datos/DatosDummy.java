1fd65efe23615f49fe8af7464310e5f1c58ffd5d
LuisAHC <luishceciliano@gmail.com>
2022-02-17 02:14:02 -0600

    agregado pabellon de prueba

--- src/test/java/com/ibm/academia/apirest/datos/DatosDummy.java
+++ src/test/java/com/ibm/academia/apirest/datos/DatosDummy.java
@@ -5,20 +5,22 @@ import java.math.BigDecimal;
 import com.ibm.academia.apirest.enumeradores.TipoEmpleado;
 import com.ibm.academia.apirest.modelos.entidades.*;
 
+import static java.lang.Long.valueOf;
+
 public class DatosDummy {
 	public static Carrera carrera01() 
 	{
-		return new Carrera(new Long(0), "Ingenieria en Sistemas", 50, 5, "nsegura");
+		return new Carrera(valueOf(0), "Ingenieria en Sistemas", 50, 5, "nsegura");
 	}
 
 	public static Carrera carrera02() 
 	{
-		return new Carrera(new Long(1), "Licenciatura en Sistemas", 45, 4, "nsegura");
+		return new Carrera(valueOf(1), "Licenciatura en Sistemas", 45, 4, "nsegura");
 	}
 
 	public static Carrera carrera03() 
 	{
-		return new Carrera(new Long(2), "Ingenieria Industrial", 60, 5, "nsegura");
+		return new Carrera(valueOf(2), "Ingenieria Industrial", 60, 5, "nsegura");
 	}
 	
 	public static Persona empleado01() {return new Empleado(null, "25147036", "Lautaro", "Lopez", new Direccion(), new BigDecimal("46750"), TipoEmpleado.ADMINISTRATIVO, "nsegura");}
@@ -35,4 +37,6 @@ public class DatosDummy {
 	public static Persona alumno02() { return new Alumno(null, "Andres", "Benitez", "45233891", new Direccion(), "nsegura");}
 
 	public static Persona alumno03() {return new Alumno(null, "Joaquin", "Leon", "45233012", new Direccion(), "nsegura");}
+
+	public static Pabellon pabellon01() { return new Pabellon(valueOf(0), 500.0, "Kala", new Direccion(), "luisceciliano"); }
 }
