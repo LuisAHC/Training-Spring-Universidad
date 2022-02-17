9d8a9131fb689dd4b4f97d4ebdd98f3ace93cc8c
LuisAHC <luishceciliano@gmail.com>
2022-02-16 23:44:48 -0600

    Agregados Tests

---
+++ src/test/java/com/ibm/academia/apirest/datos/DatosDummy.java
@@ -0,0 +1,38 @@
+package com.ibm.academia.apirest.datos;
+
+import java.math.BigDecimal;
+
+import com.ibm.academia.apirest.enumeradores.TipoEmpleado;
+import com.ibm.academia.apirest.modelos.entidades.*;
+
+public class DatosDummy {
+	public static Carrera carrera01() 
+	{
+		return new Carrera(new Long(0), "Ingenieria en Sistemas", 50, 5, "nsegura");
+	}
+
+	public static Carrera carrera02() 
+	{
+		return new Carrera(new Long(1), "Licenciatura en Sistemas", 45, 4, "nsegura");
+	}
+
+	public static Carrera carrera03() 
+	{
+		return new Carrera(new Long(2), "Ingenieria Industrial", 60, 5, "nsegura");
+	}
+	
+	public static Persona empleado01() {return new Empleado(null, "25147036", "Lautaro", "Lopez", new Direccion(), new BigDecimal("46750"), TipoEmpleado.ADMINISTRATIVO, "nsegura");}
+	
+	public static Persona empleado02() {return new Empleado(null, "25174630", "Lenadro", "Lopez", new Direccion(), new BigDecimal("46750.70"), TipoEmpleado.MANTENIMIENTO, "nsegura");}
+	
+	public static Persona profesor01() {return new Profesor(null, "4477899", "Martin", "Axacar", new Direccion(), new BigDecimal("600000"), "nsegura");}
+	
+	public static Persona alumno01()
+	{
+		return new Alumno(null, "Jhon", "Benitez", "4223715", new Direccion(), "nsegura");
+	}
+	
+	public static Persona alumno02() { return new Alumno(null, "Andres", "Benitez", "45233891", new Direccion(), "nsegura");}
+
+	public static Persona alumno03() {return new Alumno(null, "Joaquin", "Leon", "45233012", new Direccion(), "nsegura");}
+}
---
+++ src/test/java/com/ibm/academia/apirest/servicios/AlumnoDAOImplTest.java
@@ -0,0 +1,28 @@
+package com.ibm.academia.apirest.servicios;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
+import com.ibm.academia.apirest.repositorios.AlumnoRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class AlumnoDAOImplTest {
+	@MockBean
+	private AlumnoRepository alumnoRepository;
+	
+	@Autowired
+	private AlumnoDAO alumnoDao;
+	
+	@Test
+	void buscarAlumnosPorNombreCarrera() {
+		// When
+		alumnoDao.buscarAlumnoPorNombreCarrera(anyString());
+
+		// Then
+		verify(alumnoRepository).buscarAlumnoPorNombreCarrera(anyString());
+	}
+}
