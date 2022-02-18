6e12b77b1609c5a78d001b469478fa90b16761fc
LuisAHC <luishceciliano@gmail.com>
2022-02-18 02:07:47 -0600

    agregadas tres aulas

--- src/test/java/com/ibm/academia/apirest/datos/DatosDummy.java
+++ src/test/java/com/ibm/academia/apirest/datos/DatosDummy.java
@@ -3,6 +3,7 @@ package com.ibm.academia.apirest.datos;
 import java.math.BigDecimal;
 
 import com.ibm.academia.apirest.enumeradores.TipoEmpleado;
+import com.ibm.academia.apirest.enumeradores.TipoPizarron;
 import com.ibm.academia.apirest.modelos.entidades.*;
 
 import static java.lang.Long.valueOf;
@@ -38,5 +39,11 @@ public class DatosDummy {
 
 	public static Persona alumno03() {return new Alumno(null, "Joaquin", "Leon", "45233012", new Direccion(), "nsegura");}
 
-	public static Pabellon pabellon01() { return new Pabellon(valueOf(0), 500.0, "Kala", new Direccion(), "luisceciliano"); }
+	public static Pabellon pabellon01() { return new Pabellon(valueOf(1), 500.0, "Kala", new Direccion("Caminos", "12", "0000", "000", "21211", "Barcelona"), "luisceciliano"); }
+
+	public static Aula aula01() { return new Aula(valueOf(1), 1, 50.0, 18, TipoPizarron.PIZARRA_BLANCA, pabellon01(), "luisceciliano"); }
+
+	public static Aula aula02() { return new Aula(valueOf(2), 2, 55.0, 19, TipoPizarron.PIZARRA_TIZA, pabellon01(), "luisceciliano"); }
+
+	public static Aula aula03() { return new Aula(valueOf(3), 3, 70.0, 11, TipoPizarron.PIZARRA_BLANCA, pabellon01(), "luisceciliano"); }
 }
